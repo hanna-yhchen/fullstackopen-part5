58e350f7d5c99b5699736965a2907fceb88635de
Hanna Chen <hanna.yhchen@gmail.com>
2024-02-16 14:51:46 +0800

    Add token-storage and logout features

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,10 +4,21 @@ import LoginForm from '@/components/LoginForm'
 import Blog from '@/components/Blog'
 import blogService from '@/services/blogs'
 
+const LOGGED_USER_KEY = 'loggedBloglistAppUser'
+
 const App = () => {
   const [blogs, setBlogs] = useState([])
   const [user, setUser] = useState(null)
   const [notification, setNotification] = useState(null)
+
+  useEffect(() => {
+    const loggedUser = window.localStorage.getItem(LOGGED_USER_KEY)
+    if (loggedUser) {
+      const user = JSON.parse(loggedUser)
+      blogService.setToken(user.token)
+      setUser(user)
+    }
+  }, [])
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -17,11 +28,20 @@ const App = () => {
 
   const handleLoggedInUser = (user) => {
     showNotification(`Welcome back, ${user.name}!`, NotificationType.SUCCESS)
+    window.localStorage.setItem(LOGGED_USER_KEY, JSON.stringify(user))
+    blogService.setToken(user.token)
     setUser(user)
   }
 
   const handleErrorMessage = (message) => {
     showNotification(message, NotificationType.ERROR)
+  }
+
+  const handleLogoutButtonClicked = () => {
+    window.localStorage.removeItem(LOGGED_USER_KEY)
+    blogService.setToken(null)
+    setUser(null)
+    showNotification("You've been logged out!", NotificationType.SUCCESS)
   }
 
   const showNotification = (message, type) => {
@@ -44,7 +64,10 @@ const App = () => {
   const loggedInSection = () => (
     <div>
       <h2>Blogs</h2>
-      <p>{user.name} logged in</p>
+      <p>
+        Current User: {user.name} <br />
+        <button onClick={handleLogoutButtonClicked}>logout</button>
+      </p>
       {blogs.map(blog => <Blog key={blog.id} blog={blog} />)}
     </div>
   )
--- client/src/services/blogs.js
+++ client/src/services/blogs.js
@@ -1,10 +1,16 @@
 import axios from 'axios'
 const baseUrl = '/api/blogs'
 
+let token = null
+
+function setToken(newToken) {
+  token = newToken
+}
+
 async function getAll() {
   const request = axios.get(baseUrl)
   const response = await request
   return response.data
 }
 
-export default { getAll }
+export default { getAll, setToken }
